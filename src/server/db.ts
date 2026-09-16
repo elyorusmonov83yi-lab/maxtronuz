2ac6759a077b96e8691b5d98d3d5dd63f6e27ae9
Elyor <elyor.usmonov83yi@gmail.com>
2026-09-16 16:40:47 +0500

    Fix Aiven database SSL

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,27 +1,29 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-// .env faylidagi maxfiy ma'lumotlarni o'qishni faollashtiramiz
 dotenv.config();
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER,         // Faqat .env dan oladi
-  password: process.env.DB_PASSWORD, // Faqat .env dan oladi
-  database: process.env.DB_NAME,     // Faqat .env dan oladi
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 100
 });
 
-// Базага уланишни текшириш учун тест:
 db.getConnection()
   .then((conn) => {
-    console.log('✅ MariaDB базасига муваффақиятли уланди!');
+    console.log('✅ Aiven MariaDB bazasiga muvaffaqiyatli ulandi!');
     conn.release();
   })
   .catch((err) => {
-
-    console.error('❌ MariaDB уланишда хатолик:', err.message);
+    console.error('❌ MariaDB ulanishda xatolik:', err.message);
   });
